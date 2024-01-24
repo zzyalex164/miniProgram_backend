0cecb580748339183054f10afdba638f992358f3
helios <zzyalex164@gmail.com>
2024-01-24 15:28:21 +0800

    fix bugs

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ class User(db.Model):
     __tablename__ = "user"
 
     id = db.Column(db.String(32), primary_key=True)
-    username = db.Columb(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     email = db.Column(db.String(64), unique=True, index=True)
     wechat_openid = db.Column(db.String(64), unique=True, index=True)
