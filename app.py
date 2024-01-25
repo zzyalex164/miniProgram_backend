6aae614d485a629cd2152edc15993c323bce9714
helios <zzyalex164@gmail.com>
2024-01-25 20:02:10 +0800

    implement updating user info

--- app.py
+++ app.py
@@ -28,16 +28,27 @@ class User(db.Model):
     email = db.Column(db.String(128), unique=True)
     wechat_openid = db.Column(db.String(128), unique=True)
     admin = db.Column(db.Boolean, default=False)
+    name = db.Column(db.String(128))
+    telephone = db.Column(db.String(128))
+    department = db.Column(db.String(128))
+    introduction = db.Column(db.String(255))
 
 
-class OralImages(db.Model):
-    __tablename__ = "oral_images"
+class OralReport(db.Model):
+    __tablename__ = "report"
 
     user_id = db.Column(db.String(128), db.ForeignKey("user.id"))
-    file_id = db.Column(db.String(128), primary_key=True)
+    report_id = db.Column(db.String(128), primary_key=True)
     upload_time = db.Column(db.DateTime, default=datetime.now)
     description = db.Column(db.String(255))
     check_time = db.Column(db.DateTime)
+
+
+class OralImage(db.Model):
+    __tablename__ = "images"
+
+    image_id = db.Column(db.String(128), primary_key=True)
+    report_id = db.Column(db.String(128), db.ForeignKey("report.report_id"))
 
 
 def set_password(password):
@@ -122,39 +133,96 @@ def upload():
     if data is None:
         return jsonify({"msg": "No Data", "flag": False})
     user_id = data["user_id"]
-    file_id = data["file_id"]
-    id = str(uuid.uuid4())
-    img_info = OralImages(id=id, user_id=user_id, file_id=file_id)
-    db.session.add(img_info)
+    file_ids = data["file_id"]
+    report_id = str(uuid.uuid4())
+    report_info = OralReport(user_id=user_id, report_id=report_id)
+    db.session.add(report_info)
+    for file_id in file_ids:
+        img_info = OralImage(image_id=file_id, report_id=report_id)
+        db.session.add(img_info)
     db.session.commit()
     return jsonify({"msg": "Upload Success", "flag": True})
 
 
-@app.route("/api/get_images", methods=["GET"])
-def get_images(user_id):
+@app.route("/api/get_reports", methods=["GET"])
+def get_reports():
     user_id = request.args.get("user_id", None)
     if user_id is None:
         return jsonify({"msg": "No User ID", "flag": False})
-    img_list = OralImages.query.filter_by(user_id=user_id).all()
-    img_list = [
+    report_list = OralReport.query.filter_by(user_id=user_id).all()
+    report_list = [
         {
-            "file_id": img.file_id,
-            "img_path": img.upload_time,
-            "checked": img.img_desc is None,
+            "report_id": report.report_id,
+            "upload_time": report.upload_time,
+            "checked": report.description is None,
         }
-        for img in img_list
+        for report in report_list
     ]
-    return jsonify({"msg": "Get Images Success", "img_list": img_list, "flag": True})
+    return jsonify(
+        {"msg": "Get Reports Success", "report_list": report_list, "flag": True}
+    )
 
 
-@app.route("/api/get_image", methods=["GET"])
-def get_image():
-    img_id = request.args.get("img_id", None)
-    if img_id is None:
-        return jsonify({"msg": "No Image ID", "flag": False})
-    img_info = OralImages.query.filter_by(id=img_id).first()
-    img_info = {"file_id": img_info.file_id, "description": img_info.description}
-    return jsonify({"msg": "Get Image Success", "img_info": img_info, "flag": True})
+@app.route("/api/get_basic_info", methods=["GET"])
+def get_basic_info():
+    user_id = request.args.get("user_id", None)
+    if user_id is None:
+        return jsonify({"msg": "No User ID", "flag": False})
+    user_info = User.query.filter_by(id=user_id).first()
+    return jsonify(
+        {
+            "msg": "Get Basic Info Success",
+            "name": user_info.name,
+            "telephone": user_info.telephone,
+            "department": user_info.department,
+            "introduction": user_info.introduction,
+            "flag": True,
+        }
+    )
+
+
+@app.route("/api/update_basic_info", methods=["POST"])
+def update_basic_info():
+    data = request.get_json()
+    if data is None:
+        return jsonify({"msg": "No Data", "flag": False})
+    user_id = data["user_id"]
+    name = data["name"]
+    telephone = data["telephone"]
+    department = data["department"]
+    introduction = data["introduction"]
+    user_info = User.query.filter_by(id=user_id).first()
+    user_info.name = name
+    user_info.telephone = telephone
+    user_info.department = department
+    user_info.introduction = introduction
+    db.session.commit()
+    return jsonify({"msg": "Update Basic Info Success", "flag": True})
+
+
+@app.route("/api/get_report", methods=["GET"])
+def get_report():
+    report_id = request.args.get("report_id", None)
+    if report_id is None:
+        return jsonify({"msg": "No Report ID", "flag": False})
+    report_info = OralReport.query.filter_by(report_id=report_id).first()
+    images = OralImage.query.filter_by(report_id=report_id).all()
+    report_info = {
+        "report_id": report_info.report_id,
+        "upload_time": report_info.upload_time,
+        "description": report_info.description,
+        "check_time": report_info.check_time,
+        "images": [image.image_id for image in images],
+    }
+    return jsonify(
+        {"msg": "Get Report Success", "report_info": report_info, "flag": True}
+    )
+
+
+@app.route("/api/genereate_report", methods=["GET"])
+def genereate_report():
+    report_id = request.args.get("report_id", None)
+    report_info = OralReport.query.filter_by(report_id=report_id).first()
 
 
 if __name__ == "__main__":
